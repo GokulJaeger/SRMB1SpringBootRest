7639c32621b759e9bab4f5fabe0d4e8f12c10e83
GokulJaeger <gokuljaeger@gmail.com>
2021-08-09 15:26:33 +0530

    assessment5 updated

---
+++ assessment4/src/main/java/com/org/qualifier/assessment4/CrudRepository.java
@@ -0,0 +1,5 @@
+package com.org.qualifier.assessment4;
+
+public interface CrudRepository<T1, T2> {
+
+}
---
+++ assessment4/src/main/java/com/org/qualifier/assessment4/IEmployeeDao.java
@@ -0,0 +1,5 @@
+package com.org.qualifier.assessment4;
+
+public interface IEmployeeDao extends CrudRepository<Employee,Integer>{
+    
+}
